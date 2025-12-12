5a8b1762ca40a78c29b89db1fa768acf3cbb63aa
milshanfernando <milshanfernando.mf@gmail.com>
2025-12-12 12:32:08 +0400

    Refactor MongoDB connection handling to start app only after successful connection

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 const app = require("./services/express");
 const mongooseConnection = require("./services/mongoose");
 
-// mongooseConnection.start();
-// app.start();
-
-mongooseConnection
-  .start()
-  .then(() => {
-    app.start();
-  })
-  .catch((err) => {
-    console.error("MongoDB connection failed:", err);
-    process.exit(1);
-  });
+mongooseConnection.start();
+app.start();
