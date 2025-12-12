fd4dcadcd3d9b3cfd269c9a2d7cc45192aed2364
milshanfernando <milshanfernando.mf@gmail.com>
2025-12-12 12:13:06 +0400

    Fix default port in config to 8080

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 module.exports = {
-  port: process.env.PORT || 4041,
+  port: process.env.PORT || 8080,
   mongoUrl: process.env.MONGO_URL,
   cloudName: process.env.CLOUDINARY_CLOUD_NAME,
   apiKey: process.env.CLOUDINARY_API_KEY,
