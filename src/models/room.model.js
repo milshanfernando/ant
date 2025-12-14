a6c0364998d810350753b0f0ae4390b18feb8465
milshanfernando <milshanfernando.mf@gmail.com>
2025-12-14 19:29:12 +0400

    Refactor RoomSchema definition for improved readability

--- src/models/room.model.js
+++ src/models/room.model.js
@@ -1,16 +1,23 @@
 const mongoose = require("mongoose");
 
-const RoomSchema = new mongoose.Schema({
-  RoomNo: { type: String, required: true },
-  allocationList: [
-    {
-      guestName: String,
-      checkIn: Date,
-      checkOut: Date,
+const RoomSchema = new mongoose.Schema(
+  {
+    RoomNo: { type: String, required: true },
+    allocationList: [
+      {
+        guestName: String,
+        checkIn: Date,
+        checkOut: Date,
+      },
+    ],
+    active: { type: Boolean, default: true },
+    lastUpdated: {
+      type: Date,
+      default: Date.now,
     },
-  ],
-  active: { type: Boolean, default: true },
-});
+  },
+  { timestamps: true }
+);
 
 // Case-insensitive unique index
 RoomSchema.index(
