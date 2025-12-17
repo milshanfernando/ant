c5d97b10fac8d468edd2c157488a2b0beed4516c
milshanfernando <milshanfernando.mf@gmail.com>
2025-12-17 20:31:00 +0400

    Fix import path for operationalCost controller in routes

--- src/routes/api/operationalCost.routes.js
+++ src/routes/api/operationalCost.routes.js
@@ -1,7 +1,7 @@
 // routes/operationalCost.routes.js
 const express = require("express");
 const router = express.Router();
-const ctrl = require("../controllers/operationalCost.controller");
+const ctrl = require("../../controllers/operationalCost.controller");
 
 router.post("/", ctrl.createCost);
 router.get("/", ctrl.getCosts);
